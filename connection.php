a33870e1df957a9a94245e70a9ff9fd9f782d0ae
[Tejas Nikam] <[tejasnikam2505@gmail.com]>
2024-12-22 17:12:42 +0530

    Connection Updated

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $con = mysqli_connect('localhost','root','','kamilmwg_tutorial');
+    $con = mysqli_connect('localhost','	kamilmwg_myuser','Test@db24','kamilmwg_tutorial');
     if(!$con)
     {
         echo 'please check your Database connection';
