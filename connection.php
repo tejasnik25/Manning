c768fcc2fbe45a69437ad86423fde42a83ce4d52
[Tejas Nikam] <[tejasnikam2505@gmail.com]>
2025-01-09 11:07:51 +0530

    db updated

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $con = mysqli_connect('localhost','admin','admin','kamilmwg_tutorial');
+    $con = mysqli_connect('localhost','root','','kamilmwg_tutorial');
     if(!$con)
     {
         echo 'please check your Database connection';
